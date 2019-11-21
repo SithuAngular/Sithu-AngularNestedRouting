dfc079a1e8a2f6503d71cb29e97d059fbf5cde1d
Sitharthan Vk <sitharthan.v@HCL.COM>
2019-11-21 21:55:22 +0530

    [AngularNestedRouting] Created: Parent components - Level 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AttendanceComponent } from './attendance/attendance.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LeavesComponent } from './leaves/leaves.component';
+import { Page404Component } from './page404/page404.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AttendanceComponent,
+    DashboardComponent,
+    LeavesComponent,
+    Page404Component,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
